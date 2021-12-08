a19119187639c408d0e7e3571e765f7e3f7d382b
morkovinnn <dvmorkovin@miem.hse.ru>
2021-12-08 13:35:26 +0300

    v.1.0

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
 import sqlite3
 
-db = sqlite3.connect('')
+try:
+    conn = sqlite3.connect('dnevnik.db')
+    cursor = conn.cursor()
+
+    #Создадим пользователя с iser_id = 1000
+    cursor.execute("INSERT OR IGNORE INTO `users` (`user_id`) VALUES (?)", (1000,))
+
+    #Считываем всех пользователей
+    users = cursor.execute("SELECT * FROM `users`")
+    print(users.fetchall())
+
+    #Подтвердить изменения
+    conn.commit()
+
+
+except sqlite3.Error as error:
+    print('Error', error)
+
+finally:
+    if(conn):
+        conn.close()
