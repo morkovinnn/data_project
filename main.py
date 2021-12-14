7a410c01fa3c50dffd30da91cb96b7bb2cef71b3
morkovinnn <dvmorkovin@miem.hse.ru>
2021-12-14 22:18:07 +0300

    v.1.0

--- main.py
+++ main.py
@@ -1,23 +1,42 @@
 import sqlite3
 
-try:
-    conn = sqlite3.connect('dnevnik.db')
-    cursor = conn.cursor()
+class BotDB:
 
-    #Создадим пользователя с iser_id = 1000
-    cursor.execute("INSERT OR IGNORE INTO `users` (`user_id`) VALUES (?)", (1000,))
+    def __init__(self, db_file):
+        """Инициализация соединения с базой данных"""
+        self.conn = sqlite3.connect(db_file)
+        self.cursor = self.conn.cursor()
 
-    #Считываем всех пользователей
-    users = cursor.execute("SELECT * FROM `users`")
-    print(users.fetchall())
+    def user_exists(self, user_id):
+        """Проверяем существование пользователя в базе"""
+        result = self.cursor.execute("SELECT `id` FROM `users` WHERE `user_id` = ?", (user_id,))
+        return bool(len(result.fetchall()))
 
-    #Подтвердить изменения
-    conn.commit()
+    def get_user_id(self, user_id):
+        """Получаем id юзера в базе по его telegram_id"""
+        result = self.cursor.execute("SELECT `id` FROM `users` WHERE `user_id` = ?", (user_id,))
+        return result.fetchone()[0]
+
+    def add_user(self, user_id):
+        """Добавление пользователя в базу данных"""
+        self.cursor.execute("INSERT INTO `users` (`users_id`) VALUES (?)", (user_id,))
+        return self.conn.commit()
+
+    def add_name(self, user_id, users_name):
+        """Добавляем имя друга"""
+        self.cursor.execute("INSERT INTO `records` (`users_id`, `users_name` VALUES (?,?)",
+                            (self.get_user_id(user_id),
+                             operation == '+',
+                             users_name))
+        return self.conn.commit()
+
+    def get_records(self, user_id):
+        """Получаем все записи друзей"""
+        result = self.cursor.execute("SELECT * FROM `records` WHERE `user_id` = ? ORDER BY `date`",
+                                     self.get_user_id(user_id))
+        return result.fetchall()
 
 
-except sqlite3.Error as error:
-    print('Error', error)
-
-finally:
-    if(conn):
-        conn.close()
+    def close(self):
+        """Закрытие работы с базой"""
+        self.conn.close()
